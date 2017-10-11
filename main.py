f2da934e3d23702ef8785229765bc06b3a06e76c
Jayme Moehlee <jem614@gmail.com>
2017-10-11 11:01:17 -0500

    Finished working on build-a-blog

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -22,32 +23,62 @@ class Blog(db.Model):
 @app.route('/blog', methods=['POST', 'GET'])
 def blog():
 
-    if request.method == 'POST':
-        blog_title = request.form['title']
-        blog_body = request.form['body']
-        new_blog = Blog(blog_title, blog_body)
-        db.session.add(new_blog)
-        db.session.commit()
-       
+     blogs = Blog.query.all()
 
-    
-    blogs = Blog.query.all()
-    return render_template('blog.html',title="Build A Blog", 
+     blog_id = request.args.get('id')
+     if blog_id != None:
+        blog = Blog.query.get(blog_id)
+        return render_template('case_one.html', 
+        blog=blog)
+     else:
+        return render_template('blog.html',title="Build A Blog", 
         blogs=blogs)
 
+
+     
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
 
     if request.method == 'POST':
+        
         blog_title = request.form['title']
         blog_body = request.form['body']
-        new_blog = Blog(blog_title, blog_body)
-        db.session.add(new_blog)
-        db.session.commit()
 
+      
         
-    return render_template('/newpost.html')
+        if blog_title == "" or blog_body == "": 
+            return redirect('/newpost?blog_error=Please provide a blog title and blog content.')
+    
+    
+        else:
+            new_blog = Blog(blog_title, blog_body)
+            db.session.add(new_blog)
+            db.session.commit()
+            blog_id = new_blog.id 
+            return render_template('case_two.html', new_blog=new_blog,
+        blog_id=blog_id)
+
+
+    error = {'blog_error': ""}
+
+    blog_error = request.args.get("blog_error")
+    if blog_error:
+        error_esc = cgi.escape(blog_error, quote=True)
+        error['blog_error'] =  error_esc 
+
+
+
+   
+
+
+    return render_template('newpost.html', error=error)
+
+   
+
+
+
+
 
 
 if __name__ == '__main__':
